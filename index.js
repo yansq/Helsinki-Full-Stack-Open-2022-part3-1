5d98bed160e5dc9de4166f806094a7553596c5b3
yansq <yan_sq@yeah.net>
2023-02-27 16:59:48 +0800

    uibuild

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 morgan.token('body', req => JSON.stringify(req.body))
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
